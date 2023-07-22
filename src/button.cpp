fc37d405eb61c0978bd3b9c29bb26a3d4c1b2a55
Legend <me@liux.pro>
2023-07-22 17:36:13 +0800

    add more keys

--- src/button.cpp
+++ src/button.cpp
@@ -10,22 +10,27 @@ uint16_t keyStatus = 0;
 uint16_t keyLastStatus = 0xFFFF;
 
 
+static void oneKey(bsp_io_port_pin_t pinKey,uint16_t key){
+    bsp_io_level_t temp;
+    R_IOPORT_PinRead(&g_ioport_ctrl, pinKey, &temp);
+    if ((temp == BSP_IO_LEVEL_LOW) && checkBit(key, &keyLastStatus)) {
+        setBit(key, &keyStatus, true);
+    }
+    setBit(key, &keyLastStatus, temp);
+}
 
 uint16_t *keyScan() {
-
-    bsp_io_level_t temp;
-    R_IOPORT_PinRead(&g_ioport_ctrl, PIN_KEY_A, &temp);
-    if ((temp == BSP_IO_LEVEL_LOW) && checkBit(KEY_A, &keyLastStatus)) {
-        setBit(KEY_A, &keyStatus, true);
-    }
-    setBit(KEY_A, &keyLastStatus, temp);
-
-
-    R_IOPORT_PinRead(&g_ioport_ctrl, PIN_KEY_B, &temp);
-    if ((temp == BSP_IO_LEVEL_LOW) && checkBit(KEY_B, &keyLastStatus)) {
-        setBit(KEY_B, &keyStatus, true);
-    }
-    setBit(KEY_B, &keyLastStatus, temp);
+    oneKey(PIN_KEY_POWER, KEY_POWER);
+    oneKey(PIN_KEY_A, KEY_A);
+    oneKey(PIN_KEY_A_UP, KEY_A_UP);
+    oneKey(PIN_KEY_A_DOWN, KEY_A_DOWN);
+    oneKey(PIN_KEY_A_LEFT, KEY_A_LEFT);
+    oneKey(PIN_KEY_A_RIGHT, KEY_A_RIGHT);
+    oneKey(PIN_KEY_B, KEY_B);
+    oneKey(PIN_KEY_B_UP, KEY_B_UP);
+    oneKey(PIN_KEY_B_DOWN, KEY_B_DOWN);
+    oneKey(PIN_KEY_B_LEFT, KEY_B_LEFT);
+    oneKey(PIN_KEY_B_RIGHT, KEY_B_RIGHT);
     return &keyStatus;
 }
 
