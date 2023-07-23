1162e5e3373d6eebf786a806d8722557e73ab92d
Legend <me@liux.pro>
2023-07-23 22:19:20 +0800

    gomoku

--- src/app/gomoku/Gomoku.cpp
+++ src/app/gomoku/Gomoku.cpp
@@ -1,6 +1,6 @@
 #include "Gomoku.h"
 
-#define    BOARD_SIZE    15
+#define    BOARD_SIZE    13
 
 #include "./maxigo/board.h"
 
@@ -28,6 +28,9 @@ void Gomoku::drawPiece(uint8_t x, uint8_t y, piece_t piece) {
     }
 }
 
+void Gomoku::drawBox(uint8_t x, uint8_t y) {
+    u8g2.drawBox(OFFSET_X + x * GIRD - 1, y * GIRD - 1, 6, 6);
+}
 
 void Gomoku::run() {
     u8g2.setColorIndex(1);
@@ -40,18 +43,94 @@ void Gomoku::run() {
     white = &_white;
 
 
-    piece_t player = STONE_BLACK;
+    STONE_BLACK; //黑子先下
     Board board;
-    int8_t count=0;
 
+
+    bool blink = true;
+    uint8_t blinkCount = 0;
+
+    uint8_t cursorX = 0;
+    uint8_t cursorY = 0;
+    {
+        move_t move = board.negamax(1, STONE_BLACK); //ai走第一步
+        board.move(move.y, move.x, STONE_BLACK);
+    }
 
 
     while (true) {
-
-
-        move_t move = board.negamax(1, player);
-        board.move(move.y, move.x, player);
+        if (!fps_need_refresh()) {
+            continue;
+        }
+        keyScan();
         u8g2.clearBuffer();
+
+        /**
+         * 落子逻辑
+         */
+        {
+            //待选下子位置闪烁
+            if (blinkCount++ % 7 == 0) {
+                blink = !blink;
+            }
+
+            //处理按键
+            int8_t moveX = 0;
+            int8_t moveY = 0;
+            if (keyCheck(KEY_B_UP)) {
+                moveX = 0;
+                moveY = -1;
+            }
+            if (keyCheck(KEY_B_DOWN)) {
+                moveX = 0;
+                moveY = 1;
+            }
+            if (keyCheck(KEY_B_LEFT)) {
+                moveY = 0;
+                moveX = -1;
+            }
+            if (keyCheck(KEY_B_RIGHT)) {
+                moveY = 0;
+                moveX = 1;
+            }
+            if (cursorX + moveX < BOARD_SIZE and cursorX + moveX >= 0 and
+                cursorY + moveY < BOARD_SIZE and cursorY + moveY >= 0) { //防止超过棋盘
+                cursorX = cursorX + moveX;
+                cursorY = cursorY + moveY;
+                if (blink) {
+                    int8_t piece = board.signMap[cursorY][cursorX];
+                    if (piece == STONE_EMPTY) {
+                        drawPiece(cursorX, cursorY, STONE_WHITE);
+                    } else { //游标的位置有子时,绘制一个方框
+                        drawBox(cursorX, cursorY);
+                    }
+                }
+            }
+            // 画游标
+            if (keyCheck(KEY_B)) {
+                int8_t piece = board.signMap[cursorY][cursorX];
+                if (piece == STONE_EMPTY) {
+                    board.move(cursorY, cursorX, STONE_WHITE);
+                    if (board.winner_at(cursorY, cursorX)) {
+                        //白色胜
+                        printf("white");
+                    }
+                    // ai走
+                    {
+                        move_t move = board.negamax(1, STONE_BLACK);
+                        board.move(move.y, move.x, STONE_BLACK);
+                        if (board.winner_at(move.y, move.x)) {
+                            //黑色胜
+                            printf("black");
+                        }
+
+                    }
+                }
+            }
+        }
+
+
+        //画所有棋子
         for (int y = 0; y < BOARD_SIZE; ++y) { //可以优化,但没必要,反正渲染挺快的
             for (int x = 0; x < BOARD_SIZE; ++x) {
                 drawPiece(x, y, board.signMap[y][x]);
@@ -60,42 +139,43 @@ void Gomoku::run() {
         u8g2.sendBuffer();
 
 
-        if (board.winner_at(move.y, move.x)) { //
-            piece_t color = player;
-
-            int8_t dx[4] = {0, 1, 1, 1}; // 四个方向
-            int8_t dy[4] = {1, 0, 1, -1};
-
-            for (int i = 0; i < 4; i++) {
-                int8_t nx = move.x + dx[i];
-                int8_t ny = move.y + dy[i];
-                count = 1; // 已知一个(x, y)坐标在该条线上
-
-                while (board.is_in( ny,nx) && board.signMap[ny][nx] == color) {
-                    count++;
-                    nx += dx[i];
-                    ny += dy[i];
-                }
-
-                int8_t nx_ = move.x - dx[i];
-                int8_t ny_ = move.y - dy[i];
-
-                while (board.is_in( ny_,nx_) && board.signMap[ny_][nx_] == color) {
-                    count++;
-                    nx_ -= dx[i];
-                    ny_ -= dy[i];
-                }
-
-                if (count >= 5) {
-                    u8g2.drawLine((nx - dx[i])*GIRD + GIRD/2+32, (ny - dy[i])*GIRD + GIRD/2,
-                                  (nx_ + dx[i])*GIRD + GIRD/2+32, (ny_ + dy[i])*GIRD + GIRD/2);
-                    break;
-                }
-            }
-            u8g2.sendBuffer();
-            while (1);
-        }
-        player *= -1;
+//        if (board.winner_at(move.y, move.x)) { //
+//            piece_t color = player;
+//
+//            int8_t dx[4] = {0, 1, 1, 1}; // 四个方向
+//            int8_t dy[4] = {1, 0, 1, -1};
+//
+//            for (int i = 0; i < 4; i++) {
+//                int8_t nx = move.x + dx[i];
+//                int8_t ny = move.y + dy[i];
+//                count = 1; // 已知一个(x, y)坐标在该条线上
+//
+//                while (board.is_in( ny,nx) && board.signMap[ny][nx] == color) {
+//                    count++;
+//                    nx += dx[i];
+//                    ny += dy[i];
+//                }
+//
+//                int8_t nx_ = move.x - dx[i];
+//                int8_t ny_ = move.y - dy[i];
+//
+//                while (board.is_in( ny_,nx_) && board.signMap[ny_][nx_] == color) {
+//                    count++;
+//                    nx_ -= dx[i];
+//                    ny_ -= dy[i];
+//                }
+//
+//                if (count >= 5) {
+//                    u8g2.drawLine((nx - dx[i])*GIRD + GIRD/2+32, (ny - dy[i])*GIRD + GIRD/2,
+//                                  (nx_ + dx[i])*GIRD + GIRD/2+32, (ny_ + dy[i])*GIRD + GIRD/2);
+//                    u8g2.sendBuffer();
+//                    keyWaitAnyKey();
+//                    return;
+//                }
+//            }
+//        u8g2.sendBuffer();
+//        }
+//        player *= -1;
     }
 
 
--- src/app/gomoku/Gomoku.h
+++ src/app/gomoku/Gomoku.h
@@ -8,9 +8,8 @@ private:
     Sprite *white{};
     Sprite *black{};
     void drawPiece(uint8_t x, uint8_t y, int8_t piece);
+    void drawBox(uint8_t x, uint8_t y);
 
 public:
     void run() override;
-
-    void drawWinLine(int x, int y);
 };
