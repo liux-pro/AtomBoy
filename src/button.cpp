889142a041d56e6f012b987b1f08927dc314aa12
Legend <me@liux.pro>
2023-07-26 21:06:54 +0800

    power key sleep

--- src/button.cpp
+++ src/button.cpp
@@ -2,15 +2,47 @@
 #include "button.h"
 #include "utils.h"
 #include "config.h"
+#include "fps/fps.h"
+#include "sleep.h"
 
 #define delay(ms) R_BSP_SoftwareDelay(ms, BSP_DELAY_UNITS_MILLISECONDS)
 
 
-uint16_t keyStatus = 0;
-uint16_t keyLastStatus = 0xFFFF;
+uint16_t keyStatus = 0;  // bit 1 代表有按下事件
+uint16_t keyLastStatus = 0xFFFF; //保存按键实际状态
+/**
+ * 判断powerkey是长按还是短按
+ * 长按500ms重启
+ * 短按待机
+ * @param pinKey
+ */
+static void powerKey(bsp_io_port_pin_t pinKey) {
+    bsp_io_level_t temp;
+    R_IOPORT_PinRead(&g_ioport_ctrl, pinKey, &temp);
+    if (temp == BSP_IO_LEVEL_LOW) {
+            uint64_t keyTime = fps_get_ms();
+            while (true) {
+                bsp_io_level_t powerKeyStatus;
+                R_IOPORT_PinRead(&g_ioport_ctrl, pinKey, &powerKeyStatus);
+                if (powerKeyStatus == BSP_IO_LEVEL_HIGH) {
+                    break;
+                }
+            }
+            keyTime = fps_get_ms() - keyTime;
+            if (keyTime>500){
+                NVIC_SystemReset();
+            } else{
+                goSleep();
+            }
+    }
+}
 
-
-static void oneKey(bsp_io_port_pin_t pinKey,uint16_t key){
+/**
+ * 按键扫描
+ * @param pinKey
+ * @param key
+ */
+static void oneKey(bsp_io_port_pin_t pinKey, uint16_t key) {
     bsp_io_level_t temp;
     R_IOPORT_PinRead(&g_ioport_ctrl, pinKey, &temp);
     if ((temp == BSP_IO_LEVEL_LOW) && checkBit(key, &keyLastStatus)) {
@@ -20,7 +52,7 @@ static void oneKey(bsp_io_port_pin_t pinKey,uint16_t key){
 }
 
 uint16_t keyScan() {
-    oneKey(PIN_KEY_POWER, KEY_POWER);
+    powerKey(PIN_KEY_POWER);
     oneKey(PIN_KEY_A, KEY_A);
     oneKey(PIN_KEY_A_UP, KEY_A_UP);
     oneKey(PIN_KEY_A_DOWN, KEY_A_DOWN);
